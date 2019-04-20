810d0020d554bc5f8ffe3549f202a0af2df03d5c
Theo Weller <weller.theodor@gmail.com>
2019-04-20 01:14:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ function capitalize(string) {return string.toUpperCase()};
 
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + capitalize(currentUser) + '!';
 
+const welcome = 'Welcome to Flatbook, ' + currentUser + '!';
 // Write your code in this file!
