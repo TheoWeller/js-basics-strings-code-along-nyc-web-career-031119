4cfba4f9b593a7752226b87eb8eb9c169138cc1e
Theo Weller <weller.theodor@gmail.com>
2019-04-20 01:08:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const currentUser = 'Theo';
 
 const welcomeMessage = 'Welcome to Flatbook, ';
 
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';// Write your code in this file!
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser;// Write your code in this file!
