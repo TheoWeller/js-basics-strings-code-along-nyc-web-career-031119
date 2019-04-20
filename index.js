0fb0baeeb220ce381d41d1baf9a28618a5aa93ca
Theo Weller <weller.theodor@gmail.com>
2019-04-20 01:16:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function capitalize(string) {return string.toUpperCase()};
 
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + capitalize(currentUser) + '!';
 
-const welcome = 'Welcome, ' + currentUser + '!';
+const shortGreeting = 'Welcome, ' + currentUser + '!';
 // Write your code in this file!
